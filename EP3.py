06815928adb885dd5d63df20aba91b8d1fbc8e0d
thegentil <nicogentil@gmail.com>
2015-04-13 08:57:48 -0300

    Versão 3
    arrumando o readlines e organizando a lista do usuário

--- EP3.py
+++ EP3.py
@@ -8,12 +8,23 @@ Projeto 3
 
 """
 
-alimentos = open('alimentos.csv')
-usuario = open('usuario.csv',encoding="utf-8")
+alimentos = open('alimentos.csv', encoding="latin1")          #Abrindo a lista de alimentos
+usuario = open('usuario.csv',encoding="latin1")          #Abrindo a lista do usuário
 
-x =alimentos.readlines()
-y=usuario.readlines()
+a = alimentos.readlines()          #Lendo a lista de alimentos
+u = usuario.readlines()          #Lendo a lista do usuário
 
-print(x)
-print(y)
+#Organizando a lista do usuário
+
+partes = u[1].strip().split(",")          #Fazendo com que as palavras de uma linha fiquem em linhas separadas
+
+for p in partes:
+    print(p)
+        
+print('')
+
+for l in u[3:]:          #Criando uma timeline de alimentos ingeridos pelo usuário
+    for pedaço in l.strip().split(","):
+        print(pedaço)
+
 
