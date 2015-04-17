194fedae9522e82ba317f3dbc4517f4201541555
thegentil <nicogentil@gmail.com>
2015-04-17 11:10:38 -0300

    Versão 5000

--- EP3.py
+++ EP3.py
@@ -9,6 +9,12 @@ Projeto 3
 """
 
 from formulas_utilizadas import *
+import matplotlib.pyplot as plt
+
+
+'''
+Abrindo e lendo os arquivos
+'''
 
 alimentos = open('alimentos.csv', encoding="latin1")          #Abrindo a lista de alimentos
 usuario = open('usuario.csv',encoding="latin1")          #Abrindo a lista do usuário
@@ -16,13 +22,17 @@ usuario = open('usuario.csv',encoding="latin1")          #Abrindo a lista do usu
 a = alimentos.readlines()          #Lendo a lista de alimentos
 u = usuario.readlines()          #Lendo a lista do usuário
 
-#Criando os dicionários
+'''
+Criando os dicionários
+'''
 
 dic_al = {}  
 dic_gramas = {}
 dic_dias = {}
 
-#Organizando a lista do usuário
+'''
+Organizando a lista do usuário
+'''
 
 info = u[1].strip().split(",")          #Fazendo com que as palavras de uma linha fiquem em linhas separadas na vírgula
 
@@ -46,12 +56,28 @@ for l in u[3:]:
 lista = sorted(lista)
 
 print('')
-#adicionando os elementos no dic_al:
+
+'''
+Organizando a lista de alimentos
+
+Pedaço: 
+
+1 = gramas
+2 = kcalorias
+3 = proteínas
+4 = carboidratos
+5 = gorduras
+'''
+
+#Adicionando os elementos no dic_al:
 
 for e in a[1:]:
     pedacos = e.strip().split(",")
     dic_al[pedacos[0]] = [float(pedacos[1]), float(pedacos[2]), float(pedacos[3]), float(pedacos[4]), float(pedacos[5])]
-    #pedaço 1 = gramas / 2 = kcalorias / 3 = proteínas / 4 = carboidratos / 5 = gorduras
+
+'''
+Criando o programa que irá relacionar os valores aos dias
+'''
 
 #adicionando os elementos ao dic_gramas:
 
@@ -80,6 +106,13 @@ print('')
 print(dic_dias)
 
 
+
+
+
+'''
+Determinando o número de calorias que o usuário deveria ingerir a cada dia
+''' 
+
 #Fazendo com que os elementos numéricos da lista info passem para o formato float
 
 info [1] = float(info[1])
@@ -103,5 +136,26 @@ if info[3] == 'F':
     result = formula_de_hb_f(info[2], info[4], info[1], info[5])
 
 print('KCALORIAS QUE DEVERIA CONSUMIR POR DIA:', int(result), 'kcal')
+
+
+'''
+Criando os gráficos
+'''
+
+dias = [0, 1, 2, 3, 4]
+
+plt.plot(dias, result)
+plt.axis(0, 5, 0, 3000)
+plt.ylabel('Kcal ideais/Dia')
+plt.xlabel('Dias')
+plt.title('Calorias consumidas')
+plt.show()
+
+plt.plot(dias, result)
+plt.axis(0, 5, 0, 3000)
+plt.ylabel('Kcal ideais/Dia')
+plt.xlabel('Dias')
+plt.title('Calorias consumidas')
+plt.show()
     
 
